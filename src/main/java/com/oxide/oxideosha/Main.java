e66e3103ea0b5e11ec81780b02aacd7a9abf9a63
deadman96385 <seanhoyt963@gmail.com>
2019-06-14 03:41:09 -0500

    Detect if its a player being hurt and send them a message

--- src/main/java/com/oxide/oxideosha/Main.java
+++ src/main/java/com/oxide/oxideosha/Main.java
@@ -1,7 +1,9 @@
 package com.oxide.oxideosha;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
@@ -15,6 +17,9 @@ public final class Main extends JavaPlugin {
                 for (LivingEntity entity : getServer().getWorld("world").getLivingEntities()) {
                     if (entity.getLocation().subtract(0.0D, 0.4D, 0.0D).getBlock().getType().equals(Material.STONECUTTER)) {
                         entity.damage(1.0D);
+                        if (entity instanceof Player) {
+                            entity.sendMessage(ChatColor.RED + ("Ouch! ") + ChatColor.AQUA + ("You should be more careful " + ChatColor.GOLD + entity.getName()));
+                        }
                     }
                 }
             }
