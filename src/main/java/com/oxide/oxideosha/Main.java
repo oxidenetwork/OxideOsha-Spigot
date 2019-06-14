db14159252ae5ff9e535db1b9601d73057960c43
deadman96385 <seanhoyt963@gmail.com>
2019-06-14 03:26:15 -0500

    Proof of concept
    You get damaged if you walk near the center of the stone cutter
    
    Currently the world is hardcoded

--- src/main/java/com/oxide/oxideosha/Main.java
+++ src/main/java/com/oxide/oxideosha/Main.java
@@ -1,13 +1,24 @@
 package com.oxide.oxideosha;
 
+import org.bukkit.Material;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public final class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                for (LivingEntity entity : getServer().getWorld("world").getLivingEntities()) {
+                    if (entity.getLocation().subtract(0.4D, 0.4D, 0.4D).getBlock().getType().equals(Material.STONECUTTER)) {
+                        entity.damage(1.0D);
+                    }
+                }
+            }
+        }.runTaskTimer(this, 1L, 5L);
     }
 
     @Override
