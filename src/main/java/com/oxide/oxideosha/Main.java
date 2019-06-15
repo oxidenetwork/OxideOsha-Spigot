f0022fe88fb289d92c94edf243a9dc90d775f763
deadman96385 <seanhoyt963@gmail.com>
2019-06-14 20:32:05 -0500

    Add config to have who gets hurt be configurable
    Mobs Only, Players Only, Both mobs and players

--- src/main/java/com/oxide/oxideosha/Main.java
+++ src/main/java/com/oxide/oxideosha/Main.java
@@ -3,6 +3,7 @@ package com.oxide.oxideosha;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Mob;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -17,15 +18,31 @@ public final class Main extends JavaPlugin {
         saveDefaultConfig();
         plugin = this;
 
+        String damageagainst = plugin.getConfig().getString("mob-or-player-damage");
+        double damage = plugin.getConfig().getDouble("damage-value");
+
         new BukkitRunnable() {
             @Override
             public void run() {
                 for (LivingEntity entity : getServer().getWorld("world").getLivingEntities()) {
                     if (entity.getLocation().subtract(0.0D, 0.4D, 0.0D).getBlock().getType().equals(Material.STONECUTTER)) {
-                        entity.damage(plugin.getConfig().getDouble("damage-value"));
-                        if (entity instanceof Player) {
-                            entity.sendMessage(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("damage-message").replaceAll("%player%", ((Player) entity).getPlayer().getName())));
+                        if (damageagainst.equalsIgnoreCase("mob")) {
+                            if (entity instanceof Mob) {
+                                entity.damage(damage);
+                            }
+                        } else if (damageagainst.equalsIgnoreCase("player")) {
+                            if (entity instanceof Player) {
+                                entity.damage(damage);
+                                entity.sendMessage(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("damage-message").replaceAll("%player%", ((Player) entity).getPlayer().getName())));
+                            }
+                        } else if (damageagainst.equalsIgnoreCase("both")) {
+                            entity.damage(damage);
+                            if (entity instanceof Player) {
+                                entity.sendMessage(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("damage-message").replaceAll("%player%", ((Player) entity).getPlayer().getName())));
+                            }
                         }
+
+
                     }
                 }
             }
